3f2b499d210dc8455859593620a9e756b06e782b
Luis Valdez <valdez.jds.16@gmail.com>
2019-10-18 00:35:10 +0200

    add cart ended

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from '../../core/services/message/message.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor(private messageService: MessageService) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
+
+    refreshCart() {
+        this.messageService.emit('reload cart');
+    }
 
 }
