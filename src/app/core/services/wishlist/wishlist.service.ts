c2b6a983730fc71e608b324803c8be7e0402f47a
Luis Valdez <valdez.jds.16@gmail.com>
2019-10-17 23:20:23 +0200

    start cart

--- src/app/core/services/wishlist/wishlist.service.ts
+++ src/app/core/services/wishlist/wishlist.service.ts
@@ -1,9 +1,4 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
-import {HTTP} from '@ionic-native/http/ngx';
-import {Observable} from 'rxjs';
-import {CustomModel} from '../../models/Custom';
-import {environment} from '../../../../environments/environment';
 import Axios from 'axios';
 
 @Injectable({
@@ -11,32 +6,14 @@ import Axios from 'axios';
 })
 export class WishListService {
 
-    constructor(private http: HTTP,
-                private _http: HttpClient) {
+    constructor() {
     }
 
     getAll(): any {
-        const idUser = '5da8420eead1162014121733';
         return Axios.get('user/wishlist');
     }
 
     remove(customId: string): any {
-        const userId = '5da8420eead1162014121733';
         return Axios.delete('user/wishlist/' + customId);
     }
-    /* getById(wishId: string): Observable<CustomModel> {
-         const userId = '5da72b63db16e72588f8ed10';
-         return this.http.get<CustomModel>(environment.api + 'user/' + userId + '/wishlist/' + wishId);
-     }
-
-     create(customId: string): Observable<CustomModel> {
-         const userId = '5da72b63db16e72588f8ed10';
-         return this.http.post<CustomModel>(environment.api + 'user/' + userId + '/wishlist/', {customId, userId});
-     }
-
-     remove(customId: string): any {
-         const userId = '5da72b63db16e72588f8ed10';
-         return this.http.delete(environment.api + 'user/' + userId + '/wishlist/' + customId);
-     }
-     */
 }
--- src/app/views/cart/cart.component.ts
+++ src/app/views/cart/cart.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from '../../core/services/cart/cart.service';
+import {CustomModel} from '../../core/models/Custom';
+import {MessageService} from '../../core/services/message/message.service';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+    private cartItems: CustomModel[];
 
-  ngOnInit() {}
+    constructor(private cartService: CartService,
+                private messageService: MessageService) {
+    }
+
+    ngOnInit() {
+        this.getCartItems();
+        this.messageService.listen().subscribe(
+            msg => {
+                if (msg === 'reload cart') {
+                    this.getCartItems();
+                }
+            }
+        );
+    }
+
+    getCartItems() {
+        this.cartService.getAll().then(
+            items => {
+                if (items.status === 200) {
+                    this.cartItems = items.data;
+                }
+            }
+        ).catch(
+            e => console.log(e)
+        );
+    }
 
 }
